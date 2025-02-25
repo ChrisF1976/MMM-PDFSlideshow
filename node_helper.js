14f1c8aee114d7700e72bd1f230816086e0105be
Chris <150510823+ChrisF1976@users.noreply.github.com>
2025-02-25 16:42:14 +0100

    Update node_helper.js

--- node_helper.js
+++ node_helper.js
@@ -9,7 +9,7 @@ module.exports = NodeHelper.create({
 
   socketNotificationReceived: function (notification, payload) {
     if (notification === "GET_PDF_LIST") {
-      // Build the absolute path by joining __dirname with the relative folder name.
+
       let pdfFolder = path.join(__dirname, payload);
       console.log("Reading PDF folder at:", pdfFolder);
       fs.readdir(pdfFolder, (err, files) => {
@@ -17,7 +17,7 @@ module.exports = NodeHelper.create({
           this.sendSocketNotification("PDF_LIST", { error: err.message });
           return;
         }
-        // Filter only PDF files (case-insensitive)
+
         let pdfFiles = files.filter(file => path.extname(file).toLowerCase() === ".pdf");
         this.sendSocketNotification("PDF_LIST", { pdfFiles: pdfFiles });
       });
