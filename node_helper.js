2f6649ab3ce35d4cd0940ab98a62164a67ae0bca
Chris <150510823+ChrisF1976@users.noreply.github.com>
2025-02-25 19:20:24 +0100

    Added URL support for PDF

--- node_helper.js
+++ node_helper.js
@@ -1,6 +1,8 @@
 var NodeHelper = require("node_helper");
 var fs = require("fs");
 var path = require("path");
+var https = require("https");
+var http = require("http");
 
 module.exports = NodeHelper.create({
   start: function () {
@@ -9,7 +11,6 @@ module.exports = NodeHelper.create({
 
   socketNotificationReceived: function (notification, payload) {
     if (notification === "GET_PDF_LIST") {
-
       let pdfFolder = path.join(__dirname, payload);
       console.log("Reading PDF folder at:", pdfFolder);
       fs.readdir(pdfFolder, (err, files) => {
@@ -17,9 +18,45 @@ module.exports = NodeHelper.create({
           this.sendSocketNotification("PDF_LIST", { error: err.message });
           return;
         }
-
         let pdfFiles = files.filter(file => path.extname(file).toLowerCase() === ".pdf");
         this.sendSocketNotification("PDF_LIST", { pdfFiles: pdfFiles });
+      });
+    } else if (notification === "DOWNLOAD_PDF") {
+      let pdfUrl = payload.pdfURL;
+      let downloadFolder = path.join(__dirname, "pdfURL");
+      // Create the download folder if it doesn't exist.
+      if (!fs.existsSync(downloadFolder)) {
+        fs.mkdirSync(downloadFolder);
+      }
+      let fileName = path.basename(pdfUrl);
+      let filePath = path.join(downloadFolder, fileName);
+
+      // If the file already exists, return it immediately.
+      if (fs.existsSync(filePath)) {
+        console.log("File already downloaded:", filePath);
+        this.sendSocketNotification("PDF_DOWNLOADED", { pdfFile: "pdfURL/" + fileName });
+        return;
+      }
+
+      // Choose the appropriate client based on protocol.
+      let client = pdfUrl.startsWith("https://") ? https : http;
+      let file = fs.createWriteStream(filePath);
+      client.get(pdfUrl, (response) => {
+        if (response.statusCode !== 200) {
+          this.sendSocketNotification("PDF_DOWNLOAD_ERROR", { error: "Status code: " + response.statusCode });
+          return;
+        }
+        response.pipe(file);
+        file.on("finish", () => {
+          file.close(() => {
+            console.log("Downloaded file saved to", filePath);
+            // Notify the module with the relative path to the downloaded PDF.
+            this.sendSocketNotification("PDF_DOWNLOADED", { pdfFile: "pdfURL/" + fileName });
+          });
+        });
+      }).on("error", (err) => {
+        fs.unlink(filePath, () => {}); // Remove partially downloaded file.
+        this.sendSocketNotification("PDF_DOWNLOAD_ERROR", { error: err.message });
       });
     }
   }
